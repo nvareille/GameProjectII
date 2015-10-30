38c576614d4aae49f39370b0788e37155e4f263d
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2015-10-30 13:26:58 +0900

    Add Advanced character controlling

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpController : MonoBehaviour
+{
+    private Rigidbody2D Body;
+    public bool MayJump;
+    public Vector2 JumpingForce;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Body = transform.parent.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        MayJump = true;
+    }
+
+    public void Jump()
+    {
+        if (MayJump)
+        {
+            MayJump = false;
+            Body.AddForce(JumpingForce);
+        }
+    }
+}
