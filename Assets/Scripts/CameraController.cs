783a2916e3583f3b4d7e2fcec3ad4c490331e97b
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2015-11-04 10:18:03 +0900

    Fix some script issues with CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,10 @@
 
 public class CameraController : MonoBehaviour
 {
-    private Animator Animator;
     public Transform PlayerPosition;
     public Vector3 ModifiedCoordinates;
     public Vector3 ModifiedAngles;
     public float LerpSpeed = 1;
-
-	// Use this for initialization
-	void Start ()
-	{
-	    Animator = GetComponent<Animator>();
-	}
 	
 	// Update is called once per frame
 	void Update ()
@@ -20,19 +13,4 @@ public class CameraController : MonoBehaviour
 	    transform.position = Vector3.Lerp(transform.position, PlayerPosition.position + ModifiedCoordinates, Time.deltaTime * LerpSpeed);
 	    transform.rotation = Quaternion.Euler(ModifiedAngles);
 	}
-
-    void SetFollowingObject(GameObject obj)
-    {
-        PlayerPosition = obj.transform;
-    }
-
-    void TryTakePossess()
-    {
-        Animator.SetBool("Focusing", true);
-    }
-
-    void StopTakePocess()
-    {
-        Animator.SetBool("Focusing", false);
-    }
 }
