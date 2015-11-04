b2176788dc78f8e7c332fd0a145b1bf5a7c7717f
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2015-11-04 10:16:37 +0900

    Add the following camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Animator Animator;
+    public Transform PlayerPosition;
+    public Vector3 ModifiedCoordinates;
+    public Vector3 ModifiedAngles;
+    public float LerpSpeed = 1;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = Vector3.Lerp(transform.position, PlayerPosition.position + ModifiedCoordinates, Time.deltaTime * LerpSpeed);
+	    transform.rotation = Quaternion.Euler(ModifiedAngles);
+	}
+
+    void SetFollowingObject(GameObject obj)
+    {
+        PlayerPosition = obj.transform;
+    }
+
+    void TryTakePossess()
+    {
+        Animator.SetBool("Focusing", true);
+    }
+
+    void StopTakePocess()
+    {
+        Animator.SetBool("Focusing", false);
+    }
+}
