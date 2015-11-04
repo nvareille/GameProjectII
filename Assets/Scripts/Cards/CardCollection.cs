ccfee6a9ee09858ccd5bcdb46f8406c413cb9688
Xefantion <xefantion.lyo@gmail.com>
2015-11-04 02:10:05 +0100

    re-organisation des folder + grid selection du hero part1

---
+++ Assets/Scripts/Cards/CardCollection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardCollection : MonoBehaviour {
+
+    [SerializeField]
+    private CardList m_cardList;
+    [SerializeField]
+    private int[] m_hand;
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Cards/CardSelectionPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardSelectionPanel : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_cardPrefab;
+    [SerializeField]
+    private GameObject m_grid;
+
+	// Use this for initialization
+	void Start () {
+        DisplayDeck();
+	}
+
+    void DisplayDeck()
+    {
+
+    }
+
+    public void SelectHero()
+    {
+
+    }
+}
